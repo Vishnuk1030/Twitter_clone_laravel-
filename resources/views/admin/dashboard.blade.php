421f78fecbf9abf4f86503bbb06ef18554bb2b3b
vishnu k <vishnuvichu89913@gmail.com>
2024-03-31 23:04:29 +0530

    Custom page title

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,7 @@
 @extends('layout.layout')
 
+@section('title','Admin Dashboard')
+
 @section('content')
 <h1 class="text-center">Admin panel</h1>
 @endsection
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{config('app.name')}}</title>
+    <title> @yield('title') | {{ config('app.name') }}</title>
 
     <link href="https://bootswatch.com/5/lux/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 
@@ -16,8 +16,8 @@
 
 <body>
 
-{{-- including the navbar content --}}
-@include('layout/nav')
+    {{-- including the navbar content --}}
+    @include('layout/nav')
 
     <div class="container py-4">
         {{-- Page content goes here.. --}}
