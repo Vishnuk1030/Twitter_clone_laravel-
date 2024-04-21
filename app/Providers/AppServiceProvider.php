7cf4021ddde94313cc135ca1f9031201b2a5c6bf
vishnu k <vishnuvichu89913@gmail.com>
2024-04-21 17:27:13 +0530

    added caching

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,11 +26,19 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
 
+        // cache()->forget('topUsers');
+
+        $topUsers = Cache::remember('topUsers', now()->addMinutes(5), function () {
+            return User::withCount('ideas')
+                ->orderBy('ideas_count', 'DESC')
+                ->limit(5)->get();
+        });
+
+        Cache::flush();
+
         View::share(
             'topUsers',
-            User::withCount('ideas')
-                ->orderBy('ideas_count', 'DESC')
-                ->limit(5)->get()
+            $topUsers
         );
         // App::setLocale('es');
 
